6db5d4f4b4e8e4c0c0380e103206e01bff8a8ff0
Nishanth L <nishanth.l@aurigo.com>
2026-02-18 22:38:14 +0530

    Part 2

---
+++ AurigoActivities/client/src/App.tsx
@@ -0,0 +1,29 @@
+
+import { List,ListItem, ListItemText, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    fetch("https://localhost:5001/api/activities")
+      .then((response) => response.json())
+      .then((data) => setActivities(data));
+  }, []);
+  return (
+    <>
+      <Typography variant='h3'>
+        Aurigo Reactivities
+      </Typography>
+      <List>
+        {activities.map((activity) => (
+          <ListItem key={activity.id}>
+          <ListItemText>{activity.title}</ListItemText>
+          </ListItem>
+        ))}
+      </List>
+    </>
+  );
+}
+
+export default App;
---
+++ AurigoActivities/client/src/lib/types/index.d.ts
@@ -0,0 +1,11 @@
+type Activity = {
+  id: string;
+  title: string;
+  description: string;
+  date: string;
+  category: string;
+  city: string;
+  venue: string;
+  latitude: number;
+  longitude: number;
+};
